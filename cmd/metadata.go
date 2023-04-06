868754e68bfb2464a26fff00b7f1874c24da2931
Peter Sanford <psanford@sanford.io>
2023-04-06 11:17:02 -0700

    Add basic column metadata dumping

--- cmd/metadata.go
+++ cmd/metadata.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -19,6 +20,7 @@ func metadataCommand() *cobra.Command {
 	}
 
 	cmd.AddCommand(dumpMetadataCommand())
+	cmd.AddCommand(listColumnsCommand())
 	return &cmd
 }
 
@@ -62,6 +64,65 @@ func dumpMetaAction(cmd *cobra.Command, args []string) {
 	}
 
 	fmt.Println(string(out))
+
+	pr.ReadStop()
+	fr.Close()
+}
+
+func listColumnsCommand() *cobra.Command {
+	cmd := cobra.Command{
+		Use:   "columns <parquet-file>",
+		Short: "List columns from a parquet file",
+		Run:   listColumnsAction,
+	}
+
+	return &cmd
+}
+
+func listColumnsAction(cmd *cobra.Command, args []string) {
+	if len(args) != 1 {
+		log.Fatalf("Missing required <parquet-file> argument")
+	}
+
+	fr, err := local.NewLocalFileReader(args[0])
+	if err != nil {
+		log.Fatalf("Open %s failed: %s", args[0], err)
+	}
+
+	pr, err := reader.NewParquetReader(fr, nil, 1)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	for i, col := range pr.Footer.Schema {
+		if col.Type == nil {
+			continue
+		}
+
+		var min []byte
+		var max []byte
+		for _, rg := range pr.Footer.RowGroups {
+			meta := rg.Columns[i-1].MetaData
+			if min == nil {
+				min = meta.Statistics.MinValue
+			} else {
+				if bytes.Compare(meta.Statistics.MinValue, min) < 0 {
+					min = meta.Statistics.MinValue
+				}
+			}
+
+			if max == nil {
+				max = meta.Statistics.MaxValue
+			} else {
+				if bytes.Compare(meta.Statistics.MaxValue, max) > 0 {
+					max = meta.Statistics.MaxValue
+				}
+			}
+		}
+
+		fmt.Printf("%s %s %q %q\n", col.Name, col.Type, min, max)
+	}
 
 	pr.ReadStop()
 	fr.Close()
